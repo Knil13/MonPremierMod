dbcecb6969b8669b097811f1f0108b0d9c795ddf
knil_ <knil_@PC-SALON.home>
2024-11-09 23:18:35 +0100

    Ajout de la nouvelle classe ModItem afin de creer mes items moddé.

--- src/main/java/fr/knil/monpremiermod/MonPremierMod.java
+++ src/main/java/fr/knil/monpremiermod/MonPremierMod.java
@@ -5,12 +5,15 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.knil.monpremiermod.item.ModItem;
+
 public class MonPremierMod implements ModInitializer {
 	public static final String MOD_ID = "monpremiermod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		
+		ModItem.registerModItems();
 	}
 }
---
+++ src/main/java/fr/knil/monpremiermod/item/package-info.java
@@ -0,0 +1,1 @@
+package fr.knil.monpremiermod.item;
